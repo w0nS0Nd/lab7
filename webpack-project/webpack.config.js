dda5f42355eb37ac32df4496138d5eba0b0c1a69
w0nS0Nd <dimzyrgamer@gmail.com>
2025-06-07 03:14:27 +0300

    Update webpack.config.js

--- webpack-project/webpack.config.js
+++ webpack-project/webpack.config.js
@@ -4,53 +4,38 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js', // Точка входу
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'), // Вихідна папка
-        clean: true, // Очищення dist перед кожним запуском
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(jpg|jpeg|png|gif|svg|webp)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'assets/images/[name].[hash][ext][query]',
-                },
-            },
-        ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/pages/index.html',
-            filename: 'index.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/pages/rozklad.html',
-            filename: 'rozklad.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/pages/news.html',
-            filename: 'news.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/pages/photo.html',
-            filename: 'photo.html',
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'src/assets/images', to: 'assets/images' },
-            ],
-        }),
-    ],
-    mode: 'development',
-    devServer: {
-        open: true,
-        port: 8080,
-        static: {
-            directory: path.join(__dirname, 'dist'),
-        },
-    },
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/pages/index.html',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/photo.html',
+      filename: 'photo.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/news.html',
+      filename: 'news.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/rozklad.html',
+      filename: 'rozklad.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/assets/images', to: 'assets/images' }
+      ]
+    })
+  ],
+  devServer: {
+    static: './dist',
+    open: true,
+    port: 8080,
+  },
 };
